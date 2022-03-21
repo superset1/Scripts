cdffeb14775d36f57a4fe83b00211ffbe43a8240
superset1 <super_set@mail.ru>
2022-03-21 18:11:16 +0200

    7scr edited

--- 7scr_linux_runner_for_gitlab.sh
+++ 7scr_linux_runner_for_gitlab.sh
@@ -60,7 +60,21 @@ if [[ -z $DOCKER_IMAGE ]]; then
 fi
 
 case $EXECUTOR in
-     "docker") ARGUMENTS="--docker-image $DOCKER_IMAGE --docker-cpus 1 --docker-memory 1g";;
+     "docker") ARGUMENTS="--docker-image $DOCKER_IMAGE --docker-cpus 1 --docker-memory 1g"
+               # Install Docker if not exist
+               if ! [[ -f /usr/bin/docker ]]; then
+                    apt-get remove docker docker-engine docker.io containerd runc
+                    apt-get update
+                    apt-get install -y ca-certificates curl gnupg lsb-release
+                    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+                    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+                         $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+                    apt-get update
+                    apt-get install -y docker-ce docker-ce-cli containerd.io
+                    # Or instead you can use the following commands
+                    # curl -fsSL https://get.docker.com -o get-docker.sh
+                    # DRY_RUN=1 sh ./get-docker.sh
+               fi;;
     *|"shell") EXECUTOR="shell";;
 esac
 
