291aea9c5140c1206edfeac9bba0235d72cdff41
superset1 <super_set@mail.ru>
2022-03-21 20:50:14 +0200

    7scr edited

--- 7scr_linux_runner_for_gitlab.sh
+++ 7scr_linux_runner_for_gitlab.sh
@@ -30,32 +30,32 @@ if ! [[ -f /usr/local/bin/gitlab-runner ]]; then
         # Install and run as a service
         gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
         gitlab-runner start
-        
+
         # Moving .bash_logout so that gitlab does not give an error
-        mv /home/gitlab-runner/.bash_logout /home/gitlab-runner/.bash_logout.bak 
+        mv /home/gitlab-runner/.bash_logout /home/gitlab-runner/.bash_logout.bak
 fi
 
-if [[ -z $SSL ]]; then
+if [[ "$SSL" != [123] ]]; then
       read -n1 -p "Which linux gitlab-runner do you want to add? [ 1 - SSL / 2 - IgnoreSSL / 3 - NoSSL ]: " SSL
+      echo
         if  [[ $SSL != [123] ]]; then
-             echo -e "\nYou must select an encryption type!"
+             echo "You must select the correct type of encryption!"
              exit
-         elif [[ $SSL == 3 ]]; then
-             PROTOCOL="http"
         fi
-      echo
+ elif [[ "$SSL" == 3 ]]; then
+      PROTOCOL="http"
+      PORT=80
 fi
 
-if [[ -z $REGISTRATION_TOKEN ]]; then
+if [[ -z "$REGISTRATION_TOKEN" ]]; then
       read -p "Enter registration token: " REGISTRATION_TOKEN
-        if  [[ -z $REGISTRATION_TOKEN ]]; then
+        if  [[ -z "$REGISTRATION_TOKEN" ]]; then
              echo -e "\nYou must enter registration token!"
-             exit     
+             exit
         fi
-      echo
 fi
 
-if [[ -z $DOCKER_IMAGE ]]; then
+if [[ -z "$DOCKER_IMAGE" ]]; then
       DOCKER_IMAGE="ubuntu:20.04"
 fi
 
@@ -78,13 +78,13 @@ case $EXECUTOR in
     *|"shell") EXECUTOR="shell";;
 esac
 
-ARGUMENTS="--non-interactive --url ${PROTOCOL}://${SERVER}/ --registration-token $REGISTRATION_TOKEN \
+ARGUMENTS="--non-interactive --url ${PROTOCOL}://${SERVER}:${PORT}/ --registration-token $REGISTRATION_TOKEN \
            --executor $EXECUTOR $ARGUMENTS --tag-list $(hostname),$EXECUTOR"
 DESCRIPTION="$(hostname) $EXECUTOR"
 
-if [[ $SSL == [13] ]]; then
+if [[ "$SSL" == [13] ]]; then
       gitlab-runner register $ARGUMENTS --description "$DESCRIPTION"
- elif [[ $SSL == 2 ]]; then
+ elif [[ "$SSL" == 2 ]]; then
       # Create the certificates hierarchy expected by gitlab
       mkdir -p $(dirname "$CERTIFICATE")
       # Get the certificate in PEM format and store it
