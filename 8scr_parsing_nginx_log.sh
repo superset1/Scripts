56ce7a114b73a6aa75c7f5d2268905d94ec00608
superset1 <super_set@mail.ru>
2022-03-27 01:21:48 +0200

    8scr edited     add request_counter variable     changed time definition

--- 8scr_parsing_nginx_log.sh
+++ 8scr_parsing_nginx_log.sh
@@ -5,6 +5,7 @@
 
 request_timer=120
 unblock_timer=600
+request_counter=10
 
 # Adding tracking logs for the iptables
 if ! [[ -f /etc/rsyslog.d/iptables.conf ]]; then
@@ -27,7 +28,7 @@ fi
 
 # Reading entries in the nginx_log file for the last 2 minutes
 while read nginx_log; do
-     nginx_log_time=$(echo $nginx_log | cut -d " " -f 1-4 | cut -d ":" -f 2-4)
+     nginx_log_time=$(echo $nginx_log | sed -e 's/\[\|\// /g' | sed 's/:/ /' | cut -d " " -f 4-8)
        if [[ $(date +%s)-$(date --date="$nginx_log_time" '+%s') -lt "$request_timer" ]]; then
           nginx_log_2m+=$(echo -e "\n$nginx_log")
         else break  
@@ -39,7 +40,7 @@ iptables_ip=$(iptables -S | grep -oE "INPUT.*--dports 80,443 -j DROP" | awk '{pr
 nginx_log_ip=$(echo "$nginx_log_2m"| awk '{print $1}' | sort)
 # Blocking IP addresses in the iptable if there are more than 10 requests in the last 2 minutes
 for nginx_log_ip_uniq in $(echo "$nginx_log_ip" | uniq); do
-     if [[ $(grep -c "$nginx_log_ip_uniq" <<< "$nginx_log_ip") -gt 10 ]] && [[ "$iptables_ip" != *"$nginx_log_ip_uniq"* ]]; then
+     if [[ $(grep -c "$nginx_log_ip_uniq" <<< "$nginx_log_ip") -gt "$request_counter" ]] && [[ "$iptables_ip" != *"$nginx_log_ip_uniq"* ]]; then
            iptables -I INPUT -p tcp -m multiport --dport 80,443 -s $nginx_log_ip_uniq -j DROP
            iptables -I INPUT -p tcp -m multiport --dport 80,443 -s $nginx_log_ip_uniq -m limit --limit 6/min -j LOG --log-prefix "Iptables: Block ip scan: "          
      fi
@@ -48,7 +49,7 @@ done
 
 # Reading entries in the iptables_log file for the last 10 minutes
 while read iptables_log; do
-     iptables_log_time=$(echo $iptables_log | awk '{print $3}')
+     iptables_log_time=$(echo $iptables_log | cut -d " " -f 1-3)
        if [[ $(date +%s)-$(date --date="$iptables_log_time" '+%s') -lt "$unblock_timer" ]]; then
              iptables_log_10m+=$(echo -e "\n$iptables_log")
         else break  
